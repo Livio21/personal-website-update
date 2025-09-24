7faa5a18db8bf1283a38918f9261c9750bd6074a
Livio Macaj <livio.macaj@cit.edu.al>
2025-09-24 10:19:47 +0000

    Small improvements

--- src/app/home/animated-headline.tsx
+++ src/app/home/animated-headline.tsx
@@ -1,7 +1,5 @@
 "use client"
-
 import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
 import { cn } from '@/lib/utils';
 
 const words = ['design', 'develop', 'make'];
